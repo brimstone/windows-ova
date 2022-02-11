fe67882c84843546f9f2bf90dad49ba67bbaeea0
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-11 14:36:59 +0800

    Delete iso directory

--- iso/install.cmd
+++
@@ -1,1 +0,0 @@
-powershell -ExecutionPolicy ByPass -File d:\install.ps1
--- iso/install.ps1
+++
@@ -1,5 +0,0 @@
-cd $Env:USERPROFILE\Desktop
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://download.mozilla.org/?product=firefox-latest&os=win&lang=en-US", "$Env:USERPROFILE\Desktop\firefox.exe")
-.\firefox.exe -ms | Out-Null
-del firefox.exe
